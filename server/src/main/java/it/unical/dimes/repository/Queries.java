a21def87a2fc1c218f5de3916acb6c21aea1fdfc
Pietro Marsico <marsico.pietro@yahoo.it>
2025-12-08 19:20:47 +0100

    Aggiunte query mancanti: UPDATE e DELETE

--- server/src/main/java/it/unical/dimes/repository/Queries.java
+++ server/src/main/java/it/unical/dimes/repository/Queries.java
@@ -1,5 +1,6 @@
 package it.unical.dimes.repository;
 
+import it.unical.dimes.entity.Film;
 import it.unical.dimes.entity.FilmFilter;
 import it.unical.dimes.entity.SortBy;
 import it.unical.dimes.entity.ViewingStatus;
@@ -7,7 +8,7 @@ import it.unical.dimes.entity.ViewingStatus;
 import java.util.List;
 
 public class Queries {
-    private static final String TABLE_FILM = "Film";
+    private static final String TABLE = "Film";
     //nomi colonne
     private static final String ID = "id";
     private static final String TITLE = "title";
@@ -17,13 +18,18 @@ public class Queries {
     private static final String RATING = "rating";
     private static final String STATUS = "viewing_status";
 
-    public static final String INSERT = "INSERT INTO " + TABLE_FILM +
+    public static final String INSERT = "INSERT INTO " + TABLE +
             String.format("(%s, %s, %s, %s, %s, %s)", TITLE, DIRECTOR, YEAR, GENRE, RATING, STATUS) +
             " VALUES (?, ?, ?, ?, ?, ?)";
 
+    public static final String UPDATE = "UPDATE " + TABLE + " SET "
+            + String.format("%s = ?,%s = ?,%s = ?,%s = ?,%s = ?,%s = ? WHERE %s = ?",TITLE,DIRECTOR,YEAR,GENRE,RATING,STATUS,ID);
+
+    
+    public static final String DELETE = "DELETE FROM "+TABLE+" WHERE ?";
 
     public static String buildSearchQuery(FilmFilter filter, List<Object> params){
-        StringBuilder query = new StringBuilder("SELECT * FROM "+TABLE_FILM+" WHERE 1=1");
+        StringBuilder query = new StringBuilder("SELECT * FROM "+TABLE+" WHERE 1=1");
 
         if(!filter.getTitle().isEmpty()){
             query.append(" AND "+TITLE+" LIKE ? ");
