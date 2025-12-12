73b04920f70a35d45041e1ff1fe7c03880128baa
Pietro Marsico <marsico.pietro@yahoo.it>
2025-12-12 12:40:33 +0100

    Integra pull remoto e ripristina il lavoro in corso

---
+++ server/src/main/java/it/unical/dimes/TemporaryClass.java
@@ -0,0 +1,17 @@
+package it.unical.dimes;
+
+public class TemporaryClass {
+    private static final String TABLE_FILM = "Film";
+    //nomi colonne
+    private static final String ID = "id";
+    private static final String TITLE = "title";
+    private static final String DIRECTOR = "director";
+    private static final String YEAR = "year_of_release";
+    private static final String GENRE = "genre";
+    private static final String RATING = "rating";
+    private static final String STATUS = "viewing_status";
+
+    public static void main(String[] args) {
+
+    }
+}
